cf557b69f8aad480eae43591205a5e6ee5c3a06b
Bryann Alfaro <alf19372@uvg.edu.gt>
2021-09-11 18:57:49 -0600

    feat: reading first implementations

---
+++ lexer.py
@@ -0,0 +1,73 @@
+from ply import lex
+import ply.yacc as yacc
+
+tokens = (
+    'LETTERS',
+    'NEGATIVE',
+    'AND',
+    'OR',
+    'IMPLICATION',
+    'DIMPLICATION',
+    'LPAREN',
+    'RPAREN',
+    'COMMA',
+    'CONST'
+)
+
+t_ignore = ' \t'
+
+t_LPAREN  = r'\('
+t_RPAREN  = r'\)'
+t_LETTERS = r'[p-z]'
+t_CONST = r'[0-1]'
+t_COMMA = r'\,'
+t_AND = r'\^'
+t_OR = r'o'
+t_NEGATIVE = r'\~'
+t_IMPLICATION = r'\=>'
+t_DIMPLICATION = r'\<=>'
+
+def t_error( t ):
+  print("Invalid Token:",t.value[0])
+  t.lexer.skip( 1 )
+
+
+lexer = lex.lex()
+
+precedence = (
+    ('left', 'OR' ),
+    ( 'left', 'AND' ),
+    ( 'left', 'NEGATIVE' ),
+)
+
+
+
+
+def p_neg3( p ) :
+    'expr : LETTERS'
+    p[0] = p[1]
+
+def p_neg2( p ) :
+    'expr : NEGATIVE expr'
+    p[0] = p[2]
+
+def p_par(p):
+    'expr : LPAREN expr RPAREN'
+    p[0] = p[2]
+
+def p_andexp(p):
+    'expr : expr AND expr'
+    p[0] = p[2]
+
+def p_orexp(p):
+    'expr : expr OR expr'
+    p[0] = p[2]
+
+
+def p_error( p ):
+    print("Syntax error in input!")
+
+parser = yacc.yacc()
+
+res = parser.parse("(~(p^(qor))os)")
+print(res)
